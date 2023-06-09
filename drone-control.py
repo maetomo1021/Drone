f7aa97fb333103365d09386299bc37b36d4cb17a
tomoki maeda <maetomodapgmail.com>
2023-06-09 12:21:11 +0900

    テスト

--- drone-control.py
+++ drone-control.py
@@ -1,18 +1,32 @@
+#!/usr/bin/env python
+
 import socket
 # ソケットはTCP通信行う上で必要となる
 import time
 
-
-serversocket = socket.socket(socket.AF_INET,socket.SOCK)
-serversocket.bind(("localhost",8080))
-serversocket = listen(1)
-# listen()はストリーム・ソケットのみに対応しています
-# クライアントの接続待機
-
-(clientsocket,address) = serversocket.accept()
-# クライアントの接続を許可する
-
-msg = clientsocket.send(b"hello client")
-clientsocket.close()
-
-
+import socket
+import time
+ 
+#Create a UDP socket
+socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+tello_address = ('192.168.10.1' , 8889)
+ 
+#command-mode : 'command'
+socket.sendto('command'.encode('utf-8'),tello_address)
+print ('start')
+ 
+socket.sendto('takeoff'.encode('utf-8'),tello_address)
+print ('takeoff')
+ 
+time.sleep(5)
+socket.sendto(' forward 200'.encode('utf-8'),tello_address)
+time.sleep(5)
+socket.sendto('right 100'.encode('utf-8'),tello_address)
+time.sleep(5)
+socket.sendto(' back 200'.encode('utf-8'),tello_address)
+time.sleep(5)
+socket.sendto('left 100'.encode('utf-8'),tello_address)
+ime.sleep(5)
+ 
+socket.sendto('land'.encode('utf-8'),tello_address)
+print ('land')
